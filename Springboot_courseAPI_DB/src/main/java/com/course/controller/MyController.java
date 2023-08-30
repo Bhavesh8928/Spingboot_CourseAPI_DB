7bf63767569ad874270f63511f73a0b806074fcc
Bhavesh8928 <bhavesh.rs.245@gmail.com>
2023-08-30 20:25:34 +0530

    checking my controller file

--- Springboot_courseAPI_DB/src/main/java/com/course/controller/MyController.java
+++ Springboot_courseAPI_DB/src/main/java/com/course/controller/MyController.java
@@ -13,7 +13,7 @@ public class MyController {
 	@Autowired  
 	private CourseService courseServ;
 	
-	// Checking application running or not running
+	// Checking application running or not 
 	@GetMapping("/")	//http://localhost:8083/home
 	public String home() {
 		return "Welcome to courses application";
